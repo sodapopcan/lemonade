e3e4dc7844a198f6297f9c2e31d9f9366f253312
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-09 10:12:48 -0500

    Add Wallaby

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Lemonade.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:wallaby, "~> 0.26.2", [runtime: false, only: :test]}
     ]
   end
 
