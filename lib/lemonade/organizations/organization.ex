204d28d4dc8eff1eca5571b66b651a45bc3d98a5
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-11 10:11:01 -0500

    Improve creating organizations even more

--- lib/lemonade/organizations/organization.ex
+++ lib/lemonade/organizations/organization.ex
@@ -6,7 +6,7 @@ defmodule Lemonade.Organizations.Organization do
     field :name, :string
     belongs_to :created_by, Lemonade.Accounts.User
     belongs_to :owned_by, Lemonade.Accounts.User
-    field :creating_user_id, :binary_id, virtual: true
+    field :creating_user, :map, virtual: true
 
     timestamps()
   end
@@ -14,16 +14,16 @@ defmodule Lemonade.Organizations.Organization do
   @doc false
   def create_changeset(organization, attrs) do
     organization
-    |> cast(attrs, [:name, :creating_user_id])
-    |> validate_required([:name, :creating_user_id])
+    |> cast(attrs, [:name, :creating_user])
+    |> validate_required([:name, :creating_user])
     |> put_user()
   end
 
   defp put_user(changeset) do
-    creating_user_id = get_change(changeset, :creating_user_id)
+    creating_user = get_change(changeset, :creating_user)
 
     changeset
-    |> put_change(:owned_by_id, creating_user_id)
-    |> put_change(:created_by_id, creating_user_id)
+    |> put_change(:owned_by, creating_user)
+    |> put_change(:created_by, creating_user)
   end
 end
--- test/lemonade/organizations_test.exs
+++ test/lemonade/organizations_test.exs
@@ -8,7 +8,7 @@ defmodule Lemonade.OrganizationsTest do
       user = create(:user)
 
       {:ok, organization} =
-        Organizations.create_organization(%{name: "Planet Express", creating_user_id: user.id})
+        Organizations.create_organization(%{name: "Planet Express", creating_user: user})
 
       assert %{created_by_id: user_id, owned_by_id: user_id} = organization
       assert user_id == user.id
