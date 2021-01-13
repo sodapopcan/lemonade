04d0c816525ca98a6f40f02b6d34097bdfc665a4
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-12 20:24:57 -0500

    Show an error on the field

--- lib/lemonade_web/live/setup_live.ex
+++ lib/lemonade_web/live/setup_live.ex
@@ -6,12 +6,12 @@ defmodule LemonadeWeb.SetupLive do
 
   @default_assigns [
     current_user: nil,
-    organization: nil
+    organization_errors: []
   ]
 
   def mount(_, %{"user_token" => user_token}, socket) do
     current_user = Accounts.get_user_by_session_token(user_token)
-    {:ok, assign(socket, current_user: current_user)}
+    {:ok, assign(socket, current_user: current_user, organization_errors: [])}
   end
 
   def render(assigns) do
@@ -20,9 +20,10 @@ defmodule LemonadeWeb.SetupLive do
 
     <p>To get started, enter the name of your organization.  If this is for personal use, just use your name.</p>
 
-    <%= f = form_for :organization, "#", [phx_submit: "create-organization"] %>
+    <%= f = form_for :organization, "#", phx_submit: "create-organization", errors: @organization_errors %>
       <%= label f, :name %>
       <%= text_input f, :name, phx_hook: "Focus" %>
+      <%= error_tag f, :name %>
       <%= submit "Go" %>
     </form>
 
@@ -39,8 +40,10 @@ defmodule LemonadeWeb.SetupLive do
         "create-organization",
         %{"organization" => organization_params},
         %{assigns: %{current_user: current_user}} = socket
-  ) do
-    {:ok, organization} = Organizations.create_organization(current_user, organization_params)
-    {:noreply, assign(socket, organization: organization)}
+      ) do
+    case Organizations.create_organization(current_user, organization_params) do
+      {:ok, organization} -> {:noreply, assign(socket, organization: organization)}
+      {:error, %{errors: errors}} -> {:noreply, assign(socket, organization_errors: errors)}
+    end
   end
 end
