1d1021c24cfe95cf5704b5be8903c682a234eaf2
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-16 14:02:50 -0500

    Don't show logged out confirmation

--- lib/lemonade_web/controllers/user_session_controller.ex
+++ lib/lemonade_web/controllers/user_session_controller.ex
@@ -20,7 +20,6 @@ defmodule LemonadeWeb.UserSessionController do
 
   def delete(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
     |> UserAuth.log_out_user()
   end
 end
