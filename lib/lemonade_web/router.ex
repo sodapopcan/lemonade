1c4c5036f0f7ba0a3d3c86785fa5fd60e5f2828b
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-16 20:35:20 -0500

    Change route paths

--- lib/lemonade_web/router.ex
+++ lib/lemonade_web/router.ex
@@ -47,30 +47,30 @@ defmodule LemonadeWeb.Router do
   scope "/", LemonadeWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    get "/users/register", UserRegistrationController, :new
-    post "/users/register", UserRegistrationController, :create
+    get "/register", UserRegistrationController, :new
+    post "/register", UserRegistrationController, :create
     get "/login", UserSessionController, :new
     post "/login", UserSessionController, :create
-    get "/users/reset_password", UserResetPasswordController, :new
-    post "/users/reset_password", UserResetPasswordController, :create
-    get "/users/reset_password/:token", UserResetPasswordController, :edit
-    put "/users/reset_password/:token", UserResetPasswordController, :update
+    get "/reset_password", UserResetPasswordController, :new
+    post "/reset_password", UserResetPasswordController, :create
+    get "/reset_password/:token", UserResetPasswordController, :edit
+    put "/reset_password/:token", UserResetPasswordController, :update
   end
 
   scope "/", LemonadeWeb do
     pipe_through [:browser, :require_authenticated_user]
 
-    get "/users/settings", UserSettingsController, :edit
-    put "/users/settings", UserSettingsController, :update
-    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+    get "/user-settings", UserSettingsController, :edit
+    put "/user-settings", UserSettingsController, :update
+    get "/user-settings/confirm_email/:token", UserSettingsController, :confirm_email
   end
 
   scope "/", LemonadeWeb do
     pipe_through [:browser]
 
-    delete "/users/log_out", UserSessionController, :delete
-    get "/users/confirm", UserConfirmationController, :new
-    post "/users/confirm", UserConfirmationController, :create
-    get "/users/confirm/:token", UserConfirmationController, :confirm
+    delete "/logout", UserSessionController, :delete
+    get "/user-confirm", UserConfirmationController, :new
+    post "/users-confirm", UserConfirmationController, :create
+    get "/users-confirm/:token", UserConfirmationController, :confirm
   end
 end
