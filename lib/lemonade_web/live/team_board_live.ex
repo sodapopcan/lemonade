f1599f469ad586d3bdf57b7f70072184619ccec3
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-16 21:01:32 -0500

    Redirect from team board if no organization

--- lib/lemonade_web/live/team_board_live.ex
+++ lib/lemonade_web/live/team_board_live.ex
@@ -1,11 +1,16 @@
 defmodule LemonadeWeb.TeamBoardLive do
   use LemonadeWeb, :live_view
 
-  alias Lemonade.Accounts
+  alias Lemonade.{Accounts, Organizations}
 
   def mount(_, %{"user_token" => user_token}, socket) do
     current_user = Accounts.get_user_by_session_token(user_token)
-    {:ok, assign(socket, current_user: current_user)}
+    organization = Organizations.get_organization_by_owner(current_user)
+    if organization do
+      {:ok, assign(socket, current_user: current_user)}
+    else
+      {:ok, redirect(socket, to: "/setup")}
+    end
   end
 
   def render(assigns) do
