53ec08a8d82d4c3fd5ef6854ddddc1eee543a745
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-17 21:14:47 -0500

    Quick prototype of standup

--- lib/lemonade_web/live/team_board_live.ex
+++ lib/lemonade_web/live/team_board_live.ex
@@ -17,7 +17,16 @@ defmodule LemonadeWeb.TeamBoardLive do
   def render(assigns) do
     ~L"""
     <%= live_component @socket, LemonadeWeb.LayoutComponent, id: "logged-in-layout", current_user: @current_user, team: @team do %>
-      <h1><%= @team.organization.name %></h1>
+      <div class="px-4 pt-2">
+        <section>
+          <h1 class="pb-2">Standup</h1>
+          <div>
+            <div class="bg-yellow-400 w-16 h-16 text-2xl rounded-full centered">
+              <%= initials(@current_user.name) %>
+            </div>
+          </div>
+        </section>
+      </div>
     <% end %>
     """
   end
