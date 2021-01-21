067c38ee9cbe3e61f1200da7d3dd0b4a71938136
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-21 09:45:39 -0500

    Extract to standup component

---
+++ lib/lemonade_web/live/components/standup_component.ex
@@ -0,0 +1,34 @@
+defmodule LemonadeWeb.StandupComponent do
+  use LemonadeWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+        <section>
+          <header class="flex flex-start items-center">
+            <h1 class="pb-2 text-xl">Standup</h1>
+            <div class="px-4 pb-2 opacity-0 group-hover:opacity-100">
+              <%= if attending_standup?(@current_team_member, @standup) do %>
+                <a href="#" phx-click="leave-standup" title="leave standup" class="leave-standup-link"><%= icon("log-out") %></a>
+              <% end %>
+            </div>
+          </header>
+          <div class="flex items-center h-20">
+            <%= for standup_member <- @standup.standup_members do %>
+              <div class="h-20 w-20 bg-yellow-300 rounded-full centered text-2xl"><%= initials(standup_member.team_member.name) %></div>
+            <% end %>
+            <%= if !attending_standup?(@current_team_member, @standup) do %>
+              <div class="flex flex-start">
+                <a href="#" phx-click="join-standup" class="join-standup-link flex flex-start items-center"><%= icon "log-in" %> <span class="ml-2">join standup</span></a>
+              </div>
+            <% end %>
+          </div>
+        </section>
+    """
+  end
+
+  defp attending_standup?(current_team_member, standup) do
+    Enum.any?(standup.standup_members, fn %{team_member: team_member} ->
+      team_member.id == current_team_member.id
+    end)
+  end
+end
--- lib/lemonade_web/live/team_board_live.ex
+++ lib/lemonade_web/live/team_board_live.ex
@@ -17,26 +17,7 @@ defmodule LemonadeWeb.TeamBoardLive do
     ~L"""
     <%= live_component @socket, LemonadeWeb.LayoutComponent, id: "logged-in-layout", current_user: @current_team_member.user, team: @team do %>
       <div class="px-4 pt-2 group">
-        <section>
-          <header class="flex flex-start items-center">
-            <h1 class="pb-2 text-xl">Standup</h1>
-            <div class="px-4 pb-2 opacity-0 group-hover:opacity-100">
-              <%= if attending_standup?(@current_team_member, @team.standup) do %>
-                <a href="#" phx-click="leave-standup" title="leave standup" class="leave-standup-link"><%= icon("log-out") %></a>
-              <% end %>
-            </div>
-          </header>
-          <div class="flex items-center h-20">
-            <%= for standup_member <- @team.standup.standup_members do %>
-              <div class="h-20 w-20 bg-yellow-300 rounded-full centered text-2xl"><%= initials(standup_member.team_member.name) %></div>
-            <% end %>
-            <%= if !attending_standup?(@current_team_member, @team.standup) do %>
-              <div class="flex flex-start">
-                <a href="#" phx-click="join-standup" class="join-standup-link flex flex-start items-center"><%= icon "log-in" %> <span class="ml-2">join standup</span></a>
-              </div>
-            <% end %>
-          </div>
-        </section>
+        <%= live_component @socket, LemonadeWeb.StandupComponent, id: "stand-up", current_team_member: @current_team_member, standup: @team.standup %>
       </div>
     <% end %>
     """
@@ -59,11 +40,5 @@ defmodule LemonadeWeb.TeamBoardLive do
     current_team_member = TeamBoard.get_current_team_member(current_team_member.user, team)
 
     {:noreply, assign(socket, team: team, current_team_member: current_team_member)}
-  end
-
-  defp attending_standup?(current_team_member, standup) do
-    Enum.any?(standup.standup_members, fn %{team_member: team_member} ->
-      team_member.id == current_team_member.id
-    end)
   end
 end
