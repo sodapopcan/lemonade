bd5e21c112481e93c32e2ab3b45fa46f8fc8ee49
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-24 20:32:15 -0500

    Add a task for seeding dev data

--- lib/mix/tasks/planex.ex
+++ lib/mix/tasks/planex.ex
@@ -1,14 +1,88 @@
 defmodule Mix.Tasks.Planex do
   use Mix.Task
 
+  @shortdoc "Info about these tasks"
   def run(_) do
-    IO.puts("Seed some data for Planet Express.")
+    IO.puts("Well, not for now")
   end
 
-  @shortdoc "Seeds development data with the Planet Express organization"
   defmodule Seed do
+    use Mix.Task
+
+    @shortdoc "Seeds development data with the Planet Express organization"
+    @requirements ["app.start"]
     def run(_) do
-      Mix.Task.run("app.start")
+      [prof | staff] = [
+        {"Professor Hubert Farnsworth", "professor"},
+        {"Leela", "leela"},
+        {"Philip J Fry", "philip"},
+        {"Bender Bending Rodríguez", "bender"},
+        {"Amy Wong", "amy"}
+      ]
+
+      Ecto.Multi.new()
+      |> Ecto.Multi.run(:user, fn _repo, _ ->
+        {name, email} = prof
+
+        Lemonade.Accounts.register_user(%{
+          name: name,
+          email: "#{email}@planetexpress.com",
+          password: "Password12345"
+        })
+      end)
+      |> Ecto.Multi.run(:organization, fn _repo, %{user: user} ->
+        Lemonade.Organizations.bootstrap_organization(
+          user,
+          %{
+            "name" => "Planet Express",
+            "teams" => [%{"name" => "Delivery Team"}]
+          }
+        )
+      end)
+      |> Ecto.Multi.run(:staff, fn _repo, %{organization: %{teams: [team | _]} = organization} ->
+        staff =
+          Enum.map(staff, fn {name, email} ->
+            {:ok, user} =
+              Lemonade.Accounts.register_user(%{
+                name: name,
+                email: "#{email}@planetexpress.com",
+                password: "Password12345"
+              })
+
+            Lemonade.Accounts.join_organization(user, organization)
+
+            {:ok, organization_member} =
+              Lemonade.Organizations.join_organization(organization, user)
+
+            {:ok, team_member} = Lemonade.Teams.join_team(team, organization_member)
+
+            # Lemonade.Teams.join_standup(team.standup |> Lemonade.Repo.preload(:standup_members), team_member)
+            user
+          end)
+
+        {:ok, staff}
+      end)
+      |> Lemonade.Repo.transaction()
+    end
+  end
+
+  defmodule Unseed do
+    use Mix.Task
+
+    @shortdoc "Destroys the planex seed"
+    @requirements ["app.start"]
+    def run(_) do
+      Ecto.Adapters.SQL.query!(Lemonade.Repo, "UPDATE users SET organization_id = null WHERE email LIKE '%@planetexpress.com'")
+
+      Ecto.Adapters.SQL.query!(
+        Lemonade.Repo,
+        "DELETE FROM organizations WHERE name = 'Planet Express'"
+      )
+
+      Ecto.Adapters.SQL.query!(
+        Lemonade.Repo,
+        "DELETE FROM users WHERE email LIKE '%@planetexpress.com'"
+      )
     end
   end
 end
