fb585e7b5b0b28d678843d4d50ac64f07b3f3f36
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-27 08:27:53 -0500

    Refactor Factory a bit

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,32 +5,34 @@ defmodule Lemonade.Factory do
   alias Lemonade.Teams.Team
 
   def build(:user) do
+    int = int()
+
     %User{
-      name: "Hubert Farnsworth",
-      email: "user#{System.unique_integer()}@example.com",
+      name: "User #{int}",
+      email: "user#{int}@example.com",
       hashed_password: Bcrypt.hash_pwd_salt("valid password")
     }
   end
 
   def build(:organization) do
     %Organization{
-      name: "Team #{System.unique_integer()}"
+      name: "Team #{int()}"
     }
   end
 
   def build(:team) do
     %Team{
-      name: "Team #{System.unique_integer()}"
+      name: "Team #{int()}"
     }
   end
 
   ## API
   #
-  def build(factory_name, attributes) do
-    factory_name |> build() |> struct(attributes)
-  end
+  def build(factory_name, attrs),
+    do: factory_name |> build() |> struct(attrs)
 
-  def create(factory_name, attributes \\ []) do
-    Repo.insert! build(factory_name, attributes)
-  end
+  def create(factory_name, attrs \\ []),
+    do: build(factory_name, attrs) |> Repo.insert!()
+
+  defp int, do: System.unique_integer([:positive])
 end
