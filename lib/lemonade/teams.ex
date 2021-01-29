22dd37712ccdb51d053051b09eea3335fdd9c363
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-29 14:06:07 -0500

    Reorganize file

--- lib/lemonade/teams.ex
+++ lib/lemonade/teams.ex
@@ -2,7 +2,7 @@ defmodule Lemonade.Teams do
   import Ecto.Query, warn: false
   alias Lemonade.Repo
 
-  alias Lemonade.Teams.{Team, TeamMember, Standups}
+  alias Lemonade.Teams.{Team, TeamMember}
 
   def get_team_by_organization(%{id: id}) do
     Team
@@ -26,18 +26,22 @@ defmodule Lemonade.Teams do
     |> Repo.insert()
   end
 
-  def subscribe(team_id) do
-    Phoenix.PubSub.subscribe(Lemonade.PubSub, "team:#{team_id}")
-  end
-
-  def broadcast({:error, _reason} = error, _event), do: error
-  def broadcast({:ok, %{team_id: team_id} = entity}, event) do
-    Phoenix.PubSub.broadcast(Lemonade.PubSub, "team:#{team_id}", {event, entity})
-    {:ok, entity}
-  end
+  alias Lemonade.Teams.Standups
 
   defdelegate get_standup_by_team(team), to: Standups
   defdelegate join_standup(standup, team_member), to: Standups
   defdelegate leave_standup(standup, team_member), to: Standups
   defdelegate shuffle_standup(standup), to: Standups
+
+  alias Lemonade.PubSub
+
+  def subscribe(team_id) do
+    Phoenix.PubSub.subscribe(PubSub, "team:#{team_id}")
+  end
+
+  def broadcast({:error, _reason} = error, _event), do: error
+  def broadcast({:ok, %{team_id: team_id} = entity}, event) do
+    Phoenix.PubSub.broadcast(PubSub, "team:#{team_id}", {event, entity})
+    {:ok, entity}
+  end
 end
