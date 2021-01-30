86ba2396c2d00082f15b33c7920d629cb59da95b
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-29 20:10:24 -0500

    Add Timex package

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Lemonade.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:phx_gen_auth, "~> 0.6", only: [:dev], runtime: false},
-      {:wallaby, "~> 0.26.2", [runtime: false, only: :test]}
+      {:wallaby, "~> 0.26.2", [runtime: false, only: :test]},
+      {:timex, "~> 3.6"}
     ]
   end
 
