7db610a5bddd52abb096742bb8eac98bc75629f1
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-30 10:53:12 -0500

    Cleanup

--- lib/lemonade_web/live/components/vacation_component.ex
+++ lib/lemonade_web/live/components/vacation_component.ex
@@ -9,8 +9,12 @@ defmodule LemonadeWeb.VacationComponent do
       <div id="vacations" phx-update="append" class="flex">
         <%= for vacation <- @vacations do %>
           <div id="<%= vacation.id %>" class="flex items-center bold text-xs mx-1 rounded shadow">
-            <div class="font-bold centered py-1 px-2 bg-gray-100"><%= initials vacation.team_member.name %></div>
-            <div class="py-1 px-2 bg-gray-50"><%= format_date_range vacation.starts_at, vacation.ends_at %></div>
+            <div class="font-bold centered py-1 px-2 bg-gray-100">
+              <%= initials vacation.team_member.name %>
+            </div>
+            <div class="py-1 px-2 bg-gray-50">
+              <%= format_date_range vacation.starts_at, vacation.ends_at %>
+            </div>
           </div>
         <% end %>
       </div>
@@ -18,21 +22,22 @@ defmodule LemonadeWeb.VacationComponent do
     """
   end
 
-  def format_date_range(starts_at, ends_at) do
+  defp format_date_range(starts_at, ends_at) do
     fstarts_at = format(starts_at, "%b %-d")
     fends_at = format_ends_at(starts_at, ends_at)
 
-    raw "#{fstarts_at}#{fends_at}"
+    raw("#{fstarts_at}#{fends_at}")
   end
 
-  defp format_ends_at(date, date = ends_at), do: ""
+  defp format_ends_at(date, date), do: ""
 
   defp format_ends_at(%{month: month}, %{month: month} = ends_at) do
     format(ends_at, "&ndash;%-d")
   end
 
-  defp format_ends_at(starts_at, ends_at) do
+  defp format_ends_at(_starts_at, ends_at) do
     format(ends_at, "&ndash;%b %-d")
   end
+
   defp format(date, pattern), do: Timex.format!(date, pattern, :strftime)
 end
