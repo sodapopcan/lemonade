d786ce37149884f5418ff561ab5f13cd97e328db
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-30 22:38:17 -0500

    Add whitespace

--- lib/lemonade_web/live/components/vacation_form_component.ex
+++ lib/lemonade_web/live/components/vacation_form_component.ex
@@ -18,16 +18,19 @@ defmodule LemonadeWeb.VacationFormComponent do
     <div class="p-4 w-96 rounded bg-yellow-400 shadow-md" phx-hook="DateRangePicker">
       <%= f = form_for @changeset, "#", phx_submit: "book-time-off", phx_target: @myself %>
         <h1>Vacation</h1>
+
         <div id="date-rage-picker-wrapper" phx-update="ignore" class="centered p-4">
           <input type="hidden" id="date-range-picker" />
           <%= hidden_input f, :starts_at, id: "vacation-starts-at" %>
           <%= hidden_input f, :ends_at, id: "vacation-ends-at" %>
         </div>
+
         <div class="flex justify-between mx-8">
           <%= labeled_radio_button f, :type, "all day" %>
           <%= labeled_radio_button f, :type, "morning" %>
           <%= labeled_radio_button f, :type, "afternoon" %>
         </div>
+
         <div class="text-right">
           <%= live_patch "Cancel", to: Routes.team_path(@socket, :index), class: "button" %>
           <button type="submit" class="button-primary bg-yellow-200">OK</button>
