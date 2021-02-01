52d908f4fdc50199f2e19f4f973b4dab66c5bdd6
Andrew Haust <andrewwhhaust@gmail.com>
2021-01-31 22:17:25 -0500

    Get presence working

---
+++ lib/lemonade/teams/team_member_presence.ex
@@ -0,0 +1,16 @@
+defmodule Lemonade.Teams.TeamMemberPresence do
+  use Phoenix.Presence,
+    otp_app: :lemonade,
+    pubsub_server: Lemonade.PubSub
+
+  def track_team_member(topic, team_member_id, data) do
+    track(self(), topic, team_member_id, data)
+  end
+
+  def list_team_member_ids(topic) do
+    list(topic)
+      |> Enum.map(fn {_id, data} ->
+        List.first(data[:metas])[:team_member_id]
+      end)
+  end
+end
--- lib/lemonade_web/live/team_live.ex
+++ lib/lemonade_web/live/team_live.ex
@@ -3,6 +3,7 @@ defmodule LemonadeWeb.TeamLive do
 
   alias Lemonade.{Accounts, Organizations, Teams}
   alias LemonadeWeb.{LayoutComponent, VacationComponent, StandupComponent}
+  alias Lemonade.Teams.TeamMemberPresence
 
   @impl true
   def mount(_, %{"user_token" => user_token}, socket) do
@@ -11,11 +12,20 @@ defmodule LemonadeWeb.TeamLive do
     organization = Organizations.get_organization_by_organization_member(current_organization_member)
     team = Teams.get_team_by_organization(organization)
     standup = Teams.get_standup_by_team(team)
-    current_team_member =
-      Teams.get_team_member_by_organization_member(team, current_organization_member)
+    current_team_member = Teams.get_team_member_by_organization_member(team, current_organization_member)
     vacations = Teams.get_vacations_by_team(team)
+    online_team_member_ids = TeamMemberPresence.list_team_member_ids("teams:#{team.id}")
 
-    if connected?(socket), do: Teams.subscribe(team.id)
+    if connected?(socket) do
+      LemonadeWeb.Endpoint.subscribe("teams:#{team.id}")
+      TeamMemberPresence.track_team_member(
+        "teams:#{team.id}",
+        current_team_member.id, %{
+          team_member_id: current_team_member.id
+        })
+
+      Teams.subscribe(team.id)
+    end
 
     {:ok,
      assign(
@@ -23,6 +33,7 @@ defmodule LemonadeWeb.TeamLive do
        organization: organization,
        current_organization_member: current_organization_member,
        current_team_member: current_team_member,
+       online_team_member_ids: online_team_member_ids,
        team: team,
        standup: standup,
        vacations: vacations,
@@ -52,6 +63,7 @@ defmodule LemonadeWeb.TeamLive do
         <%= live_component @socket, StandupComponent,
           id: "standup",
           current_team_member: @current_team_member,
+          online_team_member_ids: @online_team_member_ids,
           standup: @standup %>
       </div>
     <% end %>
@@ -74,5 +86,11 @@ defmodule LemonadeWeb.TeamLive do
 
   def handle_info({:vacation_updated, _vacation}, socket) do
     {:noreply, assign(socket, vacations: Teams.get_vacations_by_team(socket.assigns.team))}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: payload}, socket) do
+    online_team_member_ids = TeamMemberPresence.list_team_member_ids("teams:#{socket.assigns.team.id}")
+
+    {:noreply, assign(socket, :online_team_member_ids, online_team_member_ids)}
   end
 end
