4381b86e4a1186c0b4dbfde965488408fe8a0e6a
Andrew Haust <andrewwhhaust@gmail.com>
2021-02-07 13:22:50 -0500

    Prematurely add mogrify

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Lemonade.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:phx_gen_auth, "~> 0.6", only: [:dev], runtime: false},
       {:wallaby, "~> 0.26.2", [runtime: false, only: :test]},
-      {:timex, "~> 3.6"}
+      {:timex, "~> 3.6"},
+      {:mogrify, "~> 0.8.0"}
     ]
   end
 
