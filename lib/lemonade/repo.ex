2ba88439a67982aff04450211afe0028061339f0
Andrew Haust <andrewwhhaust@gmail.com>
2021-09-29 19:44:35 -0400

    Add Repo.first/1 and Repo.last/1 functions

--- lib/lemonade/repo.ex
+++ lib/lemonade/repo.ex
@@ -2,4 +2,16 @@ defmodule Lemonade.Repo do
   use Ecto.Repo,
     otp_app: :lemonade,
     adapter: Ecto.Adapters.Postgres
+
+  def first(query) do
+    query
+    |> Ecto.Query.first()
+    |> one()
+  end
+
+  def last(query) do
+    query
+    |> Ecto.Query.last()
+    |> one()
+  end
 end
